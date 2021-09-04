56647b3b51221fd9e03d3a98370b92a4416b979a
Aniket762 <aniketindian8@gmail.com>
2021-09-04 22:06:16 +0530

    Feat:Questions controller

--- models/question.js
+++ models/question.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 var question = mongoose.model('question', {
     title: { type: String },
     message: { type: String },
+    isApproved: { type: Boolean, default: false },
 })
 
 module.exports = { question }
