3326dc61671154404496704b8b758dd8f452a955
Sumana Basu <sumana.basu2001@gmail.com>
2021-09-04 23:06:22 +0530

    Feat: All questions passcode added

--- web/components/allQuestions.jsx
+++ web/components/allQuestions.jsx
@@ -1,9 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+function passCheck() {
+    if (prompt('Please enter your password', '') == 'letmein') {
+    } else {
+        alert('Wrong Password')
+        window.location = 'http://localhost:3000/questionBank'
+    }
+}
+
 function Questions() {
-    const [questions,setQuestions]=useState([]);
+    const [questions, setQuestions] = useState([])
+
     useEffect(() => {
+        passCheck()
         try {
             axios.get('http://localhost:8080/api/questions').then(function (response) {
                 // handle success
@@ -13,18 +23,17 @@ function Questions() {
             console.log(err)
         }
     }, [])
-    console.log(questions);
+    console.log(questions)
     return (
         <div>
             <h1>Hello</h1>
             {questions.map(question => {
                 return (
-                <div key={question._id}>
-                    <h1 key={question._id}>{question.title}</h1>
-                </div>
+                    <div key={question._id}>
+                        <h1 key={question._id}>{question.title}</h1>
+                    </div>
                 )
-            })
-        }
+            })}
         </div>
     )
 }
