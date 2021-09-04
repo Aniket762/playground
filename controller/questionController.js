c6cf27180107ee8ec09fe436df604ead989531a6
Aniket762 <aniketindian8@gmail.com>
2021-09-04 19:31:28 +0530

    Feat:CRUD initialized for questions

--- controller/questionController.js
+++ controller/questionController.js
@@ -1,7 +1,10 @@
 const express = require('express')
 var router = express.Router()
+var ObjectId = require('mongoose').Types.ObjectId
 
 var { question } = require('../models/question')
+
+// get all questions
 router.get('/', (req, res, next) => {
     question.find((err, response) => {
         if (!err) res.send(response)
@@ -9,6 +12,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
+// post questions
 router.post('/', (req, res) => {
     var newQuestion = {
         title: req.body.title,
@@ -20,4 +24,27 @@ router.post('/', (req, res) => {
     })
 })
 
+// update question
+router.put('moderator/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) return res.status(400).send('No question with given Id: ' + res.params.id)
+
+    var updateQuestion = {
+        title: req.body.title,
+        message: req.body.message,
+    }
+
+    question.findByIdAndUpdate(req.params.id, { $set: updateQuestion }, (err, response) => {
+        if (!err) res.send(response)
+        else console.log('Error while updating a question: ' + JSON.stringify(err, undefined, 2))
+    })
+})
+
+// delete questions
+router.delete('moderator/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) return res.status(400).send('No question with given Id: ' + res.params.id)
+    question.findByIdAndRemove(req.params.id, (err, response) => {
+        if (!err) res.send(response)
+        else console.log('Error while deleting a question: ' + JSON.stringify(err, undefined, 2))
+    })
+})
 module.exports = router
