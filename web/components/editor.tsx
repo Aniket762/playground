10e9830bed172f7b7cb09fc0fada551c586bee6f
= <aabhassao0@gmail.com>
2021-09-04 11:55:11 +0530

    Remove unnecessary files
    Signed-off-by: = <aabhassao0@gmail.com>

--- web/components/editor.tsx
+++ web/components/editor.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import styles from '../styles/Editor.module.css'
 import axios from 'axios'
 import Btn from './button'
-import '../utils/toggle'
 import {useTheme} from './themeProvider';
 
 const Editor = () => {
