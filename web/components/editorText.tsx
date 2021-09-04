529098a27dfb727af9a8f9a48f6691ad1af21c7e
= <aabhassao0@gmail.com>
2021-09-05 03:48:24 +0530

    Change editor theme with app theme
    Signed-off-by: = <aabhassao0@gmail.com>

--- web/components/editorText.tsx
+++ web/components/editorText.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useRef} from 'react'
+import {useTheme} from './themeProvider'
 
 interface PropChild {
     mode: string;
@@ -11,10 +12,13 @@ interface Props {
 }
 
 const EditorText: React.FC<Props> = ({props, code, setCode}) => {
+    const {theme} = useTheme();
     if (typeof window !== 'undefined') {
         const Ace = require('react-ace').default;
         require('ace-builds/src-noconflict/mode-c_cpp')
         require('ace-builds/src-noconflict/mode-python')
+        require('ace-builds/src-noconflict/theme-github');
+        require('ace-builds/src-noconflict/theme-monokai');
 
         const onChange = (value: string) => {
             console.log(value);
@@ -27,6 +31,7 @@ const EditorText: React.FC<Props> = ({props, code, setCode}) => {
             fontSize={"19px"}
             width={"100%"}
             value={code}
+            theme={theme === 'dark' ? 'monokai': 'github'}
             onChange={onChange}
          />)
     }
