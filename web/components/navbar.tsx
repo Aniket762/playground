3efa819f2f0e579d28f9b22aa49178e84851ff63
= <aabhassao0@gmail.com>
2021-09-05 12:14:13 +0530

    remove unused links
    Signed-off-by: = <aabhassao0@gmail.com>

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -19,19 +19,6 @@ const Navbar = () => {
             </p>
             
             <ul className="fr">
-<<<<<<< HEAD
-                <li className="ml1 hide-small-devices">
-                    <Link href="#">snapper</Link>
-                </li>
-                <li className="ml1 hide-small-devices">
-                    <Link href="#coditor">coditor</Link>
-                </li>
-                <li className="ml1 hide-small-devices">
-                    <Link href="/questionsPage">get involved</Link>
-                </li>
-=======
->>>>>>> 1fcd59b0bc572269e7b740f5a825e675568812fd
-                <li style={{ width: '50px' }} className="ml1"></li>
                 <li className={`ml1 ${styles.toggler} show-small-devices`}>
                     <DarkModeSwitch checked={theme == 'dark'} onChange={handleChange} size={28} />
                 </li>
