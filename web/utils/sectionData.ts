02581656b3aa59581585deff110a8e76166239a0
Aniket Pal <aniketindian8@gmail.com>
2021-09-05 19:13:38 +0530

    Fix: Instruction added

--- web/utils/sectionData.ts
+++ web/utils/sectionData.ts
@@ -2,7 +2,7 @@ const sectionData = {
     coditor: {
         heading: 'Want to check some handwritten code?',
         description:
-            "Don't worry, we got you covered! And it couldn't be easier. So here's the first step, choose a file and click upload. Now our code editor will help you find some bugs if any with syntax highlighting in both c++ and python. And then the final step! Click on submit to run and get the output.",
+            "Don't worry, we got you covered! And it couldn't be easier. So here's the first step, choose a file and click upload. Now our code editor will help you find some bugs if any with syntax highlighting in both c++ and python. And then the final step! Click on submit to run and get the output.Refresh the page to see the effect!",
     },
     questions: {
         heading: 'Tired of making questions?',
