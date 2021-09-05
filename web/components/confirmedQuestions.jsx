3dd223e6522cbd21f629d7ae3996ebcddc339442
= <aabhassao0@gmail.com>
2021-09-05 14:33:31 +0530

    Fix routes typo
    Signed-off-by: = <aabhassao0@gmail.com>

--- web/components/confirmedQuestions.jsx
+++ web/components/confirmedQuestions.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import styles from '../styles/Question.module.scss'
+import baseAddress from '../utils/baseAddress'
 
 function ConfirmedQuestions() {
     const [questions, setQuestions] = useState([])
