d44ccd7e2e29a12b471320fe389f9808b23b48e6
Aniket762 <aniketindian8@gmail.com>
2021-09-05 16:19:04 +0530

    Poem: Heading added

--- web/utils/sectionData.ts
+++ web/utils/sectionData.ts
@@ -1,12 +1,14 @@
 const sectionData = {
     coditor: {
-        heading: "",
-        description: "Want to check some handwritten code? Or want to run some code to get output? Don't worry, we got you covered! And it couldn't be easier. So here's the first step, choose a file and click upload. Now our code editor will help you find some bugs if any with syntax highlighting in both c++ and python. And then the final step! Click on submit to run and get the output."
+        heading: 'Want to check some handwritten code?',
+        description:
+            "Don't worry, we got you covered! And it couldn't be easier. So here's the first step, choose a file and click upload. Now our code editor will help you find some bugs if any with syntax highlighting in both c++ and python. And then the final step! Click on submit to run and get the output.",
     },
     questions: {
-        heading: "Tired of making questions?",
-        description: "We are here to help you! Actually not just us, everyone! Check out the favourite questions of the crowd across the globe. Wish to help others? Contribute to our crowd-sourcing question bank."
-    }
+        heading: 'Tired of making questions?',
+        description:
+            'We are here to help you! Actually not just us, everyone! Check out the favourite questions of the crowd across the globe. Wish to help others? Contribute to our crowd-sourcing question bank.',
+    },
 }
 
-export default sectionData;
+export default sectionData
