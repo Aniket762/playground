43da1803205592fa59fcc44611fcf913f746f83b
Sumana Basu <sumana.basu2001@gmail.com>
2021-09-05 11:06:16 +0530

    Enhancement: Navbar styled

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -15,15 +15,6 @@ const Navbar = () => {
                 <p id="logo">Playground</p>
             </a>
             <ul className="fr">
-                <li className="ml1 hide-small-devices">
-                    <a href="#">snapper</a>
-                </li>
-                <li className="ml1 hide-small-devices">
-                    <a href="#">coditor</a>
-                </li>
-                <li className="ml1 hide-small-devices">
-                    <a href="#">get involved</a>
-                </li>
                 <li style={{ width: '50px' }} className="ml1"></li>
                 <li className={`ml1 ${styles.toggler} show-small-devices`}>
                     <DarkModeSwitch checked={theme == 'dark'} onChange={handleChange} size={28} />
