1d4bc27afb3585aeeb4a07233c9535a8ac1ba645
Aniket762 <aniketindian8@gmail.com>
2021-09-05 12:32:48 +0530

    Poem: Removed build files

--- web/public/worker-development.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-// To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
-// https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
-// eslint-disable-next-line no-underscore-dangle,no-restricted-globals
-self.__WB_DISABLE_DEV_LOGS = true;
-/******/ })()
-;
